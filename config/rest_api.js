55a6a7284f741a88f9fedaf4da2acccef26c08d2
cefff <cef.ramirez@gmail.com>
2014-10-02 03:21:13 +0000

    adding people page edits from jian

--- config/rest_api.js
+++ config/rest_api.js
@@ -1,4 +1,4 @@
-var host_url = "http://ec2-54-68-51-131.us-west-2.compute.amazonaws.com:5454/ssnoc";
+var host_url = process.env.REST_API_URL || "http://ec2-54-68-51-131.us-west-2.compute.amazonaws.com:5454/ssnoc";
 
 var RestAPI = {
   'get_all_users' : host_url + '/users',
